bc5b252074b60169e5e0f0adb88145f6746aa9a2
Aidab23 <72881705+Aidab23@users.noreply.github.com>
2023-04-08 16:16:06 -0500

    Update to browser_chrome

--- index.js
+++ index.js
@@ -31,8 +31,37 @@ function launch_app(id, html, x, y, w, h) {
 
 $("#browser_chrome").click(function(){
   launch_app("browser_chrome", "apps/browser_chrome.html");
+  includeHTML()
 });
 
+
+function includeHTML() {
+  var z, i, elmnt, file, xhttp;
+  /* Loop through a collection of all HTML elements: */
+  z = document.getElementsByTagName("*");
+  for (i = 0; i < z.length; i++) {
+    elmnt = z[i];
+    /*search for elements with a certain atrribute:*/
+    file = elmnt.getAttribute("w3-include-html");
+    if (file) {
+      /* Make an HTTP request using the attribute value as the file name: */
+      xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = function() {
+        if (this.readyState == 4) {
+          if (this.status == 200) {elmnt.innerHTML = this.responseText;}
+          if (this.status == 404) {elmnt.innerHTML = "Page not found.";}
+          /* Remove the attribute, and call this function once more: */
+          elmnt.removeAttribute("w3-include-html");
+          includeHTML();
+        }
+      }
+      xhttp.open("GET", file, true);
+      xhttp.send();
+      /* Exit the function: */
+      return;
+    }
+  }
+}
 
 
 function makeResizable(element, minW = 100, minH = 100, size = 40)
